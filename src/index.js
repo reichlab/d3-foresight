152028b678f95647f98decb3c2b361411b7ed67a
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-02-04 19:16:06 -0500

    Switch to webpack. Export classes used in flusight

---
+++ src/index.js
@@ -0,0 +1,4 @@
+// Expose these to users
+
+export { default as Choropleth } from './choropleth'
+export { default as TimeChart } from './timechart'
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const webpack = require('webpack')
+const path = require('path')
+
+const PROD = JSON.parse(process.env.PROD_ENV || '0')
+
+let plugins = []
+
+if (PROD) {
+  plugins.push(new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false
+    },
+    output: {
+      comments: false
+    }
+  }))
+}
+
+const config = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+
+  output: {
+    filename: PROD ? 'd3-foresight.min.js' : 'd3-foresight.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'd3Foresight',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['latest']
+        }
+      }
+    ]
+  },
+
+  plugins: plugins
+}
+
+module.exports = config
