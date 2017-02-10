87d738e6691bb5e6ecc0e031a11d78623a334550
Juan Sanchez <mgz4@cdc.gov>
2017-02-10 16:32:59 -0400

    wip to get something to render on a stand alone dev setup

--- examples/index.js
+++ examples/index.js
@@ -1,6 +1,12 @@
 (function () {
   window.onload = function () {
-    var timechart = new window.d3Foresight.TimeChart(document.getElementById('container'), { weekHook: function () {} })
+    var timechart = new window.d3Foresight.TimeChart(document.getElementById('container'))
+    var xhr = new window.XMLHttpRequest()
+    xhr.addEventListener('load', function (data) {
+      timechart.plot(window.JSON.parse(this.responseText))
+    })
+    xhr.open('GET', 'data.json')
+    xhr.send()
     return timechart
   }
 })()
