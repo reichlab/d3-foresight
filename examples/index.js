6eed00058b02d966f9e0728d5f2d371176e5c186
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-02-11 05:01:14 -0500

    Add data for timechart

--- examples/index.js
+++ examples/index.js
@@ -1,6 +1,11 @@
 (function () {
   window.onload = function () {
-    var timechart = new window.d3Foresight.TimeChart(document.getElementById('container'), { weekHook: function () {} })
-    return timechart
+    var timechart = new window.d3Foresight.TimeChart('#chart')
+    var xhr = new window.XMLHttpRequest()
+    xhr.addEventListener('load', function (data) {
+      timechart.plot(window.JSON.parse(this.responseText))
+    })
+    xhr.open('GET', 'data-timechart.json')
+    xhr.send()
   }
 })()
