14c255afd595d1f275d1f414de3ec8df1d6ca146
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-04-27 22:28:46 -0400

    Skip basis smoothing

--- src/components/distribution-chart/prediction.js
+++ src/components/distribution-chart/prediction.js
@@ -44,7 +44,6 @@ export default class Prediction {
       }
 
       let line = d3.line()
-          .curve(d3.curveBasis)
           .x(d => parent.xScale(d[0]))
           .y(d => parent.yScale(d[1]))
 
@@ -55,7 +54,6 @@ export default class Prediction {
         .attr('d', line)
 
       let area = d3.area()
-          .curve(d3.curveBasis)
           .x(d => parent.xScale(d[0]))
           .y1(d => parent.yScale(0))
           .y0(d => parent.yScale(d[1]))
