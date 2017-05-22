3c75304e4d48e36fb9c727854baa38fc2dea1dc7
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-05-21 23:09:43 -0400

    Separate axis in two files

---
+++ src/components/common/axis-y.js
@@ -0,0 +1,46 @@
+import * as d3 from 'd3'
+import * as cutils from '../../utilities/common'
+
+/**
+ * Simple linear Y axis with informative label
+ */
+export class YAxis {
+  constructor (svg, height, xOffset, axisConfig, infoTooltip) {
+    let axis = svg.append('g')
+        .attr('class', 'axis axis-y')
+        .attr('transform', `translate(${xOffset}, 0)`)
+
+    axis.append('text')
+      .attr('class', 'title')
+      .attr('transform', `translate(-45 , ${height / 2}) rotate(-90)`)
+      .attr('dy', '.71em')
+      .style('text-anchor', 'middle')
+      .text(axisConfig.title)
+      .style('cursor', 'pointer')
+      .on('mouseover', () => infoTooltip.show())
+      .on('mouseout', () => infoTooltip.hide())
+      .on('mousemove', function () {
+        infoTooltip.renderText({
+          title: null,
+          text: axisConfig.description
+        })
+        let pos = cutils.getMousePosition(d3.select('.overlay'))
+        infoTooltip.move({
+          x: pos[0],
+          y: pos[1]
+        })
+      })
+      .on('click', () => {
+        window.open(axisConfig.url, '_blank')
+      })
+
+    this.svg = svg
+  }
+
+  plot (yScale, maxTicks) {
+    let yAxis = d3.axisLeft(yScale).tickFormat(d3.format('.2f'))
+    if (maxTicks) yAxis.ticks(maxTicks)
+    this.svg.select('.axis-y')
+      .transition().duration(200).call(yAxis)
+  }
+}
--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,3 +1,4 @@
 export { default as ControlPanel } from './control-panel'
-export { XAxis, XAxisDate, YAxis } from './axis'
+export { XAxis, XAxisDate } from './axis-x'
+export { YAxis } from './axis-y'
 export { DistributionTooltip, TimeChartTooltip, InfoTooltip } from './tooltip'
