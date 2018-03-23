b6bdd606e7966989d6a114f11dfb14bf0c5b9ae5
Abhinav Tushar <lepisma@fastmail.com>
2018-03-23 17:38:19 -0400

    Inherit overlay from scomponent

--- src/components/time-chart/overlay.js
+++ src/components/time-chart/overlay.js
@@ -1,77 +1,119 @@
 import * as d3 from 'd3'
 import * as tt from '../../utilities/tooltip'
 import * as ev from '../../events'
+import SComponent from '../s-component'
 
-export default class Overlay {
-  constructor (parent) {
-    let svg = parent.svg
-    let height = parent.height
-    let onsetHeight = parent.onsetHeight
-    let width = parent.width
-    let tooltip = parent.tooltip
-    let xScale = parent.xScale
-    let chartHeight = height + onsetHeight
-
-    // Add text for no prediction
-    this.noPredText = parent.selection.append('div')
+class NoPredText extends SComponent {
+  constructor () {
+    super()
+    this.selection.append('text')
       .attr('class', 'no-pred-text')
-      .html('Predictions not available <br> for selected time')
+      .attr('transform', 'translate(20, 20)')
+      .text('Predictions not available')
 
-    // Add mouse hover line
-    let line = svg.append('line')
-        .attr('class', 'hover-line')
-        .attr('x1', 0)
-        .attr('y1', 0)
-        .attr('x2', 0)
-        .attr('y2', chartHeight)
-        .style('display', 'none')
+    this.selection.append('text')
+      .attr('class', 'no-pred-text')
+      .attr('transform', 'translate(20, 40)')
+      .text('for selected time')
+  }
+}
 
-    // Add now line
-    let nowGroup = svg.append('g')
-        .attr('class', 'now-group')
+class HoverLine extends SComponent {
+  constructor (layout) {
+    super()
+    this.line = this.selection.append('line')
+      .attr('class', 'hover-line')
+      .attr('x1', 0)
+      .attr('y1', 0)
+      .attr('x2', 0)
+      .attr('y2', layout.totalHeight)
+  }
 
-    nowGroup.append('line')
+  set x (x) {
+    this.line
+      .transition()
+      .duration(50)
+      .attr('x1', x)
+      .attr('x2', x)
+  }
+}
+
+class TodayLine extends SComponent {
+  constructor (layout) {
+    super()
+    this.line = this.selection.append('line')
       .attr('class', 'now-line')
       .attr('x1', 0)
       .attr('y1', 0)
       .attr('x2', 0)
-      .attr('y2', chartHeight)
-    nowGroup.append('text')
+      .attr('y2', layout.totalHeight)
+
+    this.text = this.selection.append('text')
       .attr('class', 'now-text')
       .attr('transform', 'translate(15, 10) rotate(-90)')
       .style('text-anchor', 'end')
       .text('Today')
-    this.nowGroup = nowGroup
+  }
 
-    svg.append('rect')
+  set x (x) {
+    this.line
+      .attr('x1', x)
+      .attr('x2', x)
+
+    this.text
+      .attr('dy', x)
+  }
+}
+
+export default class Overlay extends SComponent {
+  constructor (layout, { tooltip, uuid }) {
+    super()
+
+    this.noPredText = this.append(new NoPredText(layout))
+    this.todayLine = this.append(new TodayLine(layout))
+    this.hoverLine = this.append(new HoverLine(layout))
+
+    this.overlay = this.selection.append('rect')
       .attr('class', 'overlay')
-      .attr('height', chartHeight)
-      .attr('width', width)
+      .attr('height', layout.totalHeight)
+      .attr('width', layout.width)
       .on('mouseover', () => {
-        line.style('display', null)
+        this.hoverLine.hidden = false
         tooltip.hidden = false
       })
       .on('mouseout', () => {
-        line.style('display', 'none')
+        this.hoverLine.hidden = true
         tooltip.hidden = true
       })
 
+    this.tooltip = tooltip
+    this.uuid = uuid
+  }
+
+  plot (scales, showNowLine, queryObjects) {
+    if (showNowLine) {
+      this.todayLine.x = scales.xScaleDate(new Date())
+      this.todayLine.hidden = false
+    } else {
+      this.todayLine.hidden = true
+    }
+
     // Add mouse move and click events
-    d3.select('.overlay')
+    let that = this
+    this.overlay
       .on('mousemove', function () {
         let mouse = d3.mouse(this)
         // Snap x to nearest tick
-        let index = Math.round(xScale.invert(mouse[0]))
-        let snappedX = xScale(index)
+        let index = Math.round(scales.xScale.invert(mouse[0]))
+        let snappedX = scales.xScale(index)
 
-        // Move the cursor
-        d3.select('.hover-line')
-          .transition()
-          .duration(50)
-          .attr('x1', snappedX)
-          .attr('x2', snappedX)
+        that.hoverLine.x = snappedX
 
-        let visiblePreds = parent.predictions.filter(p => p.query(index))
+        let visiblePreds = queryObjects.filter(q => {
+          // Take only model predictions which have data at index
+          return (['Actual', 'Observed'].indexOf(q.id) === -1) && (q.query(index))
+        })
+
         let ttTitle = ''
         if (visiblePreds.length > 0) {
           // Add note regarding which prediction is getting displayed
@@ -81,38 +123,20 @@ export default class Overlay {
           }
         }
 
-        tooltip.render(tt.parsePredictions({
+        that.tooltip.render(tt.parsePredictions({
           title: ttTitle,
-          predictions: [parent.observed, parent.actual, ...parent.predictions],
+          predictions: queryObjects,
           index
         }))
 
-        tt.moveTooltip(tooltip, d3.select(this))
+        tt.moveTooltip(that.tooltip, d3.select(this))
       })
       .on('click', function () {
-        ev.publish(parent.uuid, ev.JUMP_TO_INDEX, Math.round(xScale.invert(d3.mouse(this)[0])))
+        ev.publish(that.uuid, ev.JUMP_TO_INDEX, Math.round(scales.xScale.invert(d3.mouse(this)[0])))
       })
-  }
-
-  plot (parent, showNowLine) {
-    if (showNowLine) {
-      let nowPos = parent.xScaleDate(new Date())
-      this.nowGroup.select('.now-line')
-        .attr('x1', nowPos)
-        .attr('x2', nowPos)
-      this.nowGroup.select('.now-text')
-        .attr('dy', nowPos)
-      this.nowGroup.style('display', null)
-    } else {
-      this.nowGroup.style('display', 'none')
-    }
   }
 
   update (predictions) {
-    // Update no prediction text
-    this.noPredText
-      .transition()
-      .duration(200)
-      .style('display', (predictions.filter(p => p.noData).length !== 0) ? null : 'none')
+    this.noPredText.hidden = !predictions.every(p => p.noData)
   }
 }
