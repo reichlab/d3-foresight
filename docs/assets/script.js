da7f97450f600e8de020c0280686d9dc38df90ae
Abhinav Tushar <lepisma@fastmail.com>
2018-03-25 23:52:32 -0400

    Doc for basic plot

--- docs/assets/script.js
+++ docs/assets/script.js
@@ -1,53 +1,79 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-  let timeChartOptions = {
-    pointType: 'mmwr-week'
+document.addEventListener("DOMContentLoaded", function () {
+
+let config = {
+  pointType: 'regular-week', // This is optional since regular-week is default
+  axes: {
+    y: {
+      title: 'Random numbers' // Title for the y axis
+    }
   }
+}
 
-  let timeChart = new d3Foresight.TimeChart('#timechart', timeChartOptions)
-
-  let data = {
-    timePoints: [
-      { week: 20, year: 2016 },
-      { week: 21, year: 2016 },
-      { week: 22, year: 2016 },
-      { week: 23, year: 2016 },
-      { week: 24, year: 2016 },
-    ],
-    actual: [1.1, 1.3, 1.4, 1.3, 1.1],
-    models: [
-      {
-        id: 'mod',
-        meta: {
-          name: 'Name',
-          description: 'Model description here',
-          url: 'http://github.com'
-        },
-        predictions: [
-          null,
-          {
-            series: [
-              { point: 1.5 },
-              { point: 1.5 },
-            ]
-          },
-          {
-            series: [
-              { point: 1.5 },
-              { point: 1.5 },
-            ]
-          },
-          null,
-          null
-        ]
-      }
-    ]
-  }
-  timeChart.plot(data)
-
-  window.tc = timeChart
-  // For updating the current position of displayed predictions
-  // timeChart.update(0)
-  // Or simply
-  // timeChart.moveForward()
-  // timeChart.moveBackward()
+let timePoints = [...Array(51).keys()].map(w => {
+  return { week: w + 1, year: 2016 }
 })
+
+// Random sequence generator
+function rseq (n) {
+  let seq = [Math.random()]
+  for (let i = 1; i < n; i++) {
+    seq.push(Math.random() * (1 + seq[i - 1]))
+  }
+  return seq
+}
+
+// Predictions look like [{ series: [{ point: 0.5 }, { point: 1.2 } ...] }, ..., null, null]
+let predictions = timePoints.map(tp => {
+  if (tp.week > 30) {
+    // We only predict upto week 30
+    return null
+  } else {
+    // Provide 10 week ahead predictions
+    return {
+      series: rseq(10).map(r => { return { point: r } })
+    }
+  }
+})
+
+let data = {
+  timePoints,
+  models: [
+    {
+      id: 'mod',
+      meta: {
+        name: 'Name',
+        description: 'Model description here',
+        url: 'http://github.com'
+      },
+      predictions
+    }
+  ]
+}
+
+// 1. Initialize
+// Setup the id of div where we are going to plot
+// Also pass in config options
+let timeChart = new d3Foresight.TimeChart('#timechart', config)
+
+// 2. Plot
+// Provide the data for the complete year
+timeChart.plot(data)
+
+// 3. Update
+// Move to the given index in the set of timePoints
+timeChart.update(0)
+// Or simply use
+// timeChart.moveForward()
+// timeChart.moveBackward()
+
+// Lets also save the timechart object in global namespace
+window.timeChart = timeChart
+
+d3.select('#mouse-btn').on('click', function () {
+  eval(d3.select('#mouse-code').text())
+  d3.select(this)
+    .classed('disabled', true)
+    .text('✓ Done')
+})
+
+}) // Closing the documentation loaded function
