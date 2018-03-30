6e201b752f8928a8a32bcf941e2bf8ffa08cfe20
Abhinav Tushar <lepisma@fastmail.com>
2018-03-29 22:02:12 -0400

    Expose only JUMP_TO_INDEX

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import './styles/main.scss'
-import { JUMP_TO_INDEX, FORWARD_INDEX, BACKWARD_INDEX } from './events'
+import { JUMP_TO_INDEX } from './events'
 
 export { default as TimeChart } from './time-chart'
 export { default as DistributionChart } from './distribution-chart'
-export const events = { JUMP_TO_INDEX, FORWARD_INDEX, BACKWARD_INDEX }
+export const events = { JUMP_TO_INDEX }
