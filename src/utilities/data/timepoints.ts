57ea86df507d3264617437f16efa2057875f7fbe
Abhinav Tushar <lepisma@fastmail.com>
2018-05-06 00:39:12 -0400

    Allow js date objects as timepoints

--- src/utilities/data/timepoints.ts
+++ src/utilities/data/timepoints.ts
@@ -9,12 +9,42 @@ import * as mmwr from 'mmwr-week'
 import * as d3 from 'd3'
 import { Timepoint } from '../../interfaces'
 import * as errors from '../errors'
+import { parseText } from '../tooltip';
 
+/**
+ * Return week number for the date
+ */
+function parseWeek(d: Date): number {
+  return parseInt(d3.timeFormat('%W')(d)) + 1
+}
+
+/**
+ * Parse timepoint to standard dictionary format
+ */
+function parseTimepoint(tp: Timepoint | Date, pointType: string): Timepoint {
+  if ((<Timepoint>tp).year) {
+    // This is already in the correct form
+    return <Timepoint>tp
+  } else if (tp instanceof Date) {
+    if (pointType === 'regular-week') {
+      return { year: tp.getFullYear(), week: parseWeek(tp) }
+    } else if (pointType === 'mmwr-week') {
+      let mdate = new mmwr.MMWRDate(0)
+      mdate.fromJSDate(tp)
+      return { year: mdate.year, week: mdate.week }
+    } else if (pointType === 'biweek') {
+      return { year: tp.getFullYear(), week: Math.floor(parseWeek(tp) / 2) }
+    } else {
+      throw new errors.UnknownPointType()
+    }
+  }
+}
 
 /**
  * Return ticks to show for the timepoints
  */
-export function getTick(tp: Timepoint, pointType: string): string | number {
+export function getTick(tp: Timepoint | Date, pointType: string): string | number {
+  tp = parseTimepoint(tp, pointType)
   // @ts-ignore
   if (pointType.endsWith('-week')) {
     // This is either regular-week or mmwr-week. Both have the same structure.
@@ -29,14 +59,18 @@ export function getTick(tp: Timepoint, pointType: string): string | number {
 /**
  * Return date time value for the timepoint
  */
-export function getDateTime(tp: Timepoint, pointType: string) {
-  if (pointType === 'regular-week') {
-    return d3.timeParse('%Y-%W')(`${tp.year}-${tp.week}`)
-  } else if (pointType === 'mmwr-week') {
-    return (new mmwr.MMWRDate(tp.year, tp.week)).toMomentDate()
-  } else if (pointType === 'biweek') {
-    return d3.timeParse('%Y-%W')(`${tp.year}-${tp.biweek * 2}`)
+export function getDateTime(tp: Timepoint | Date, pointType: string) {
+  if (tp instanceof Date) {
+    return tp
   } else {
-    throw new errors.UnknownPointType()
+    if (pointType === 'regular-week') {
+      return d3.timeParse('%Y-%W')(`${tp.year}-${tp.week}`)
+    } else if (pointType === 'mmwr-week') {
+      return (new mmwr.MMWRDate(tp.year, tp.week)).toMomentDate()
+    } else if (pointType === 'biweek') {
+      return d3.timeParse('%Y-%W')(`${tp.year}-${tp.biweek * 2}`)
+    } else {
+      throw new errors.UnknownPointType()
+    }
   }
 }
